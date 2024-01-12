6e13efcca82627d897ea608712df15adc0222585
kr-alok96 <alokkr1996@gmail.com>
2024-01-12 13:57:51 +0530

    minor improvements

--- employee/views.py
+++ employee/views.py
@@ -3,8 +3,8 @@ from employee.models import Employee
 
 # Create your views here.
 def employee_management(request):
-    emp_id = ''
-    data = {'emp_id':''}
+    
+    html_data = {'emp_id':'','title':'Employee Management'}
     if request.method == 'POST':
         emp_id = request.POST.get('employee_id')
         emp_name = request.POST.get('employee_name')
@@ -12,6 +12,6 @@ def employee_management(request):
         newEmployeeData = Employee(employee_id = emp_id,employee_name = emp_name)
         newEmployeeData.save()
 
-        data = {'emp_id':emp_id}
+        html_data['emp_id'] = emp_id
 
-    return render(request,"employee-management.html",data)
+    return render(request,"employee-management.html",html_data)
--- sdms/views.py
+++ sdms/views.py
@@ -5,19 +5,24 @@ from students.models import Student
 def home(request):
     html_data = {
         'title':'Home',
-        'clist':[1,2,3]
     }
     return render(request,'home.html',html_data)
 
 def about_us(request):
-    return render(request,"about_us.html")
+    html_data = {
+        'title':'About Us',
+    }
+    return render(request,"about_us.html",html_data)
 
 def about_us_description(request,stakeholder):
     return HttpResponse("Welcome To SDMS "+stakeholder)
 
 def student_management(request):
 
+    html_data = {
+        'title': 'Student Management'
+    }
     student_data = Student.objects.all()
-    data_dictionary = {'Students':student_data} # html template expects data in dictionary format
+    html_data['Students'] = student_data # html template expects data in dictionary format
 
-    return render(request,"student_management.html",data_dictionary)
+    return render(request,"student_management.html",html_data)
